b4b3e56c59d66aec9e9dc6eeceda0f610d99d279
Julia Gorelikova <jkireenko137@gmail.com>
2021-07-14 13:59:00 +0300

    Домашние задания из третьего модуля

---
+++ 3glava/lesson2_step13.py
@@ -0,0 +1,75 @@
+import unittest
+from selenium import webdriver
+import time
+
+class TestRegistration(unittest.TestCase):
+    def test_abs1(self):
+        link = "http://suninjuly.github.io/registration1.html"
+        browser = webdriver.Chrome()
+        browser.get(link)
+
+        # Ваш код, который заполняет обязательные поля
+        input1 = browser.find_element_by_class_name("first")
+        input1.send_keys("Yla")
+        input2 = browser.find_element_by_class_name("second")
+        input2.send_keys("Pip")
+        input3 = browser.find_element_by_class_name("third")
+        input3.send_keys("jkki@mail.ru")
+
+        # Отправляем заполненную форму
+        button = browser.find_element_by_css_selector("button.btn")
+        button.click()
+
+        # Проверяем, что смогли зарегистрироваться
+        # ждем загрузки страницы
+        time.sleep(1)
+
+        # находим элемент, содержащий текст
+        welcome_text_elt = browser.find_element_by_tag_name("h1")
+        # записываем в переменную welcome_text текст из элемента welcome_text_elt
+        welcome_text = welcome_text_elt.text
+
+        # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+        self.assertEqual("Congratulations! You have successfully registered!", welcome_text, "No registered")
+
+        # ожидание чтобы визуально оценить результаты прохождения скрипта
+        time.sleep(10)
+        # закрываем браузер после всех манипуляций
+        browser.quit()
+
+    def test_abs2(self):
+        link = "http://suninjuly.github.io/registration2.html"
+        browser = webdriver.Chrome()
+        browser.get(link)
+
+        # Ваш код, который заполняет обязательные поля
+        input1 = browser.find_element_by_css_selector("div.first_block input.first")
+        input1.send_keys("Заполни")
+        input2 = browser.find_element_by_css_selector("div.first_block input.second")
+        input2.send_keys("Заполни")
+        input3 = browser.find_element_by_css_selector("div.first_block input.third")
+        input3.send_keys("Заполни")
+        # Отправляем заполненную форму
+        button = browser.find_element_by_css_selector("button.btn")
+        button.click()
+
+        # Проверяем, что смогли зарегистрироваться
+        # ждем загрузки страницы
+        time.sleep(1)
+
+        # находим элемент, содержащий текст
+        welcome_text_elt = browser.find_element_by_tag_name("h1")
+        # записываем в переменную welcome_text текст из элемента welcome_text_elt
+        welcome_text = welcome_text_elt.text
+
+        # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+        self.assertEqual("Congratulations! You have successfully registered!", welcome_text, "No registered")
+
+        # ожидание чтобы визуально оценить результаты прохождения скрипта
+        time.sleep(10)
+        # закрываем браузер после всех манипуляций
+        browser.quit()
+
+
+if __name__ == "__main__":
+    unittest.main()
